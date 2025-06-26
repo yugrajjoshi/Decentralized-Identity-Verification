1b26247deaa49f83dfd6c445a0c192a7a276e71f
yugrajjoshi <yugrajjoshi185@gmail.com>
2025-06-26 15:06:40 +0530

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contract with account:", deployer.address);
+
+  const IdentityContract = await hre.ethers.getContractFactory("DecentralizedIdentityVerification");
+  const contract = await IdentityContract.deploy();
+
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
